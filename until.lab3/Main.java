0d33afce90abd12324741200fd6789d8e9e0f95a
alonecheer <alonecheer@hotmail.com>
2021-02-01 22:42:04 +0700

    :zap: finish!

--- until.lab3/Main.java
+++ until.lab3/Main.java
@@ -6,17 +6,16 @@ public class Main {
         people.addStudent("021","Cheer",4.00);
         people.addStudent("000","Meen",3.99);
         people.addStudent("999","Chachaya",3.00);
+        people.addStudent("091","test",1.99);
         System.out.println("########## showAllStudentInfo ##########");
         people.showAllStudentInfo();
         System.out.println("########## removeStudentByCode ##########");
-        people.removeStudentByCode("021");
+        people.removeStudentByCode("888");
         System.out.println("########## showAllStudentInfo ##########");
         people.showAllStudentInfo();
         System.out.println("########## searchStudentByCode ##########");
-        System.out.println(">> Result search : "+ " " + people.searchStudentByCode("999")+ "\n"); 
+        System.out.println(">> Result search : "+ " " + people.searchStudentByCode("777")+ "\n"); 
         System.out.println("########## getNumberOfStudent ##########");
         people.getNumberOfStudent();
     }  
-
-
 }
--- until.lab3/StudentList.java
+++ until.lab3/StudentList.java
@@ -1,32 +1,20 @@
 import java.util.ArrayList;
 
 public class StudentList {
-ArrayList<Student> arrList = new ArrayList<Student>(); 
-
+    ArrayList<Student> arrList = new ArrayList<Student>(); 
     public StudentList() {  }
-
-
-    
     public void addStudent(String _code, String _name, double _gpa) {
-        
         arrList.add(new Student(_code,_name,_gpa) );
-        //System.out.println(arrList.toString());
     }
 
     public void removeStudentByCode(String _code) {
-        //System.out.println(arrList.get(0).code);	
       for (Student num : arrList) { 		      
-           //System.out.println(num.code);
            if (num.code == _code){
-            //System.out.println("find"); 
-            //System.out.println(arrList.remove(num));
             arrList.remove(num);
             System.out.println(">> Remove student code : " + num.code + " " + "done" + "\n");
             break;
            }
-           else System.out.println(">> Not find"); 	
       }
-    //   System.out.println(">> After remove "+arrList.toString() + "\n");
     }
 
     public void showAllStudentInfo() {
@@ -44,7 +32,6 @@ ArrayList<Student> arrList = new ArrayList<Student>();
             else {
                 x = false;
             };
-            
        }
        return x;
     }
@@ -52,5 +39,4 @@ ArrayList<Student> arrList = new ArrayList<Student>();
     public void getNumberOfStudent() {
         System.out.println(">> All Student : " + arrList.size() + " " + "people" );
     }
-
 }
